56fbfa6549aa4d4a8e0fbc03ebc08b6b1f93fe1b
jaqop <jaqopx1@outlook.com>
2023-12-03 11:05:16 -0800

    Update ourTime.cpp

--- c++Deep/ourTime.cpp
+++ c++Deep/ourTime.cpp
@@ -1,10 +1,26 @@
-#include <fstream>
-#include <iostream>
+// C program to demonstrate nanosleep() function
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+
+enum { SECS = 2,
+    NANOSEC = 30 };
 
 int main()
 {
-	std::cout << "here we go...";
+    struct timespec r1, r2 = { SECS, NANOSEC };
+    int cnt = 0;
 
-	 
-	return 0;
+    printf("Counter stated..\n");
+    while (cnt <= 10) {
+        printf("Counter:  %d\n", cnt);
+        if (cnt == 5) {
+            printf("Sleeping ....\n");
+            nanosleep(&r2, &r1);
+        }
+        cnt++;
+    }
+    return 0;
 }
